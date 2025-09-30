9360284e6732ecabbc6df6aa664684fabafbf827
MatthiasPohlAmberg <kontakt@pohl-matthias.com>
2025-09-30 13:39:07 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     author_email='kontakt@pohl-matthias.com',
     url='https://github.com/MatthiasPohlAmberg/VeCo.git',
     install_requires=[  # Liste der Abhängigkeiten
-        'torch==2.7.1',
+        'torch==2.8.0',
         'openai-whisper==20240930',
         'faiss-cpu==1.11.0.post1',
         'pdfplumber==0.11.7',
@@ -24,9 +24,9 @@ setup(
         'sentence-transformers==5.0.0',
     ],
     classifiers=[
-        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.12',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.6, <3.11',
+    python_requires='>=3.6, <3.12',
 )
